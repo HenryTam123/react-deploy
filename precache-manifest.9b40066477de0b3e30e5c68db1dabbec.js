9958c6c7f5501f5cfbc759e069fb99c9060cdb7c
HenryTam123 <u3557967@connect.hku.hk>
2020-08-14 13:39:03 +0800

    Updates

---
+++ precache-manifest.9b40066477de0b3e30e5c68db1dabbec.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54485449c0aba441626e83e9a1c3ea4",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "e88f4f571a28155b6a46",
+    "url": "/react-deploy/static/css/main.3115be48.chunk.css"
+  },
+  {
+    "revision": "2ad71a5926f160c797d2",
+    "url": "/react-deploy/static/js/2.96ecccbc.chunk.js"
+  },
+  {
+    "revision": "adaeb4e4187998be0a36a9d50662ea68",
+    "url": "/react-deploy/static/js/2.96ecccbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88f4f571a28155b6a46",
+    "url": "/react-deploy/static/js/main.28fb9b7c.chunk.js"
+  },
+  {
+    "revision": "6a4cc26f78389a56bfc0",
+    "url": "/react-deploy/static/js/runtime-main.e9d1a115.js"
+  },
+  {
+    "revision": "02f12c2cfb48c824674a90ef716217ba",
+    "url": "/react-deploy/static/media/book.02f12c2c.svg"
+  },
+  {
+    "revision": "d8b43294298ea5a24f22443def32a962",
+    "url": "/react-deploy/static/media/business.d8b43294.png"
+  },
+  {
+    "revision": "3b3f5532e51b5728c2276669ffd45ae0",
+    "url": "/react-deploy/static/media/calculator.3b3f5532.jpg"
+  },
+  {
+    "revision": "6d72e5e5acaf2ed740129e7ea6027851",
+    "url": "/react-deploy/static/media/clock.6d72e5e5.jpg"
+  },
+  {
+    "revision": "ab9b88998835ac23f08f8428802bd9ca",
+    "url": "/react-deploy/static/media/cover.ab9b8899.jpg"
+  },
+  {
+    "revision": "85dee36b065ae27c3313688f604af47f",
+    "url": "/react-deploy/static/media/game.85dee36b.jpg"
+  },
+  {
+    "revision": "4f46366ac488a7254412818085e37a90",
+    "url": "/react-deploy/static/media/game2.4f46366a.jpg"
+  },
+  {
+    "revision": "d37cb33afadbdffd2f2adca1c3f80605",
+    "url": "/react-deploy/static/media/game3.d37cb33a.jpg"
+  },
+  {
+    "revision": "e0340b2e54e23b02271466ce560ea0e6",
+    "url": "/react-deploy/static/media/gaming.e0340b2e.svg"
+  },
+  {
+    "revision": "be179a256244d0175da94f8a7e48e040",
+    "url": "/react-deploy/static/media/greeting.be179a25.svg"
+  },
+  {
+    "revision": "3c62c8483ae8687aacb6a288ab435be3",
+    "url": "/react-deploy/static/media/hangman.3c62c848.jpg"
+  },
+  {
+    "revision": "ce0c572eed9075eb5326825d66af35a0",
+    "url": "/react-deploy/static/media/hug.ce0c572e.png"
+  },
+  {
+    "revision": "21ad26894508ea60f8b523f3831b820f",
+    "url": "/react-deploy/static/media/image-gallery.21ad2689.jpg"
+  },
+  {
+    "revision": "681ec1bfac0bed67ad5b895eb28945a4",
+    "url": "/react-deploy/static/media/music.681ec1bf.svg"
+  },
+  {
+    "revision": "937b3cd88b0f49096aafac9f380e3c43",
+    "url": "/react-deploy/static/media/piano.937b3cd8.jpg"
+  },
+  {
+    "revision": "1f5ddf85a346fe5a0d84886a2d739f96",
+    "url": "/react-deploy/static/media/planet.1f5ddf85.svg"
+  },
+  {
+    "revision": "f206d8a9c17c01adb94272c66a8b416d",
+    "url": "/react-deploy/static/media/planet2.f206d8a9.svg"
+  },
+  {
+    "revision": "6b5c1e3598e36d43e3e94539b2ea67ca",
+    "url": "/react-deploy/static/media/quiz.6b5c1e35.jpg"
+  },
+  {
+    "revision": "10d90444cfc4477e8ca146a49c40dc29",
+    "url": "/react-deploy/static/media/sky.10d90444.jpg"
+  },
+  {
+    "revision": "24441a12a62b8137140ed7142d488b95",
+    "url": "/react-deploy/static/media/sky2.24441a12.jpg"
+  },
+  {
+    "revision": "c7795ffd79d66a7bfeda732a713539a8",
+    "url": "/react-deploy/static/media/todolist.c7795ffd.jpg"
+  },
+  {
+    "revision": "3b465b909321c90303ce7b4494bd5a73",
+    "url": "/react-deploy/static/media/ufo.3b465b90.png"
+  },
+  {
+    "revision": "e12626a8c8ce00ed7cdf2112fed98b05",
+    "url": "/react-deploy/static/media/video.e12626a8.mp4"
+  }
+]);
